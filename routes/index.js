cefec141d83318a6911e39ea195c7a72ba98b2d4
airrakeshkumarsharma <airrakeshkumarsharma@gmail.com>
2019-05-14 16:36:08 +0530

    runs part is done

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,102 @@ var router = express.Router();
 var monk = require('monk');  //get the monk javascript
 var moment = require('moment'); //get the moment data
 var dbs=monk('mongodb://admin:admin123@ds155916.mlab.com:55916/my_cricket_adda');
-var score 
+var scoreCollection = dbs.get("score");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 router.get('/admin', function(req, res, next) {
-  res.render('admin');
+    res.render('admin');
 });
 
 //score post method
 router.post('/score_update', function(req, res){
-	var score = req.body.score;
+    var score = Number(req.body.score);
+    var total_run = 0;
+    scoreCollection.findOne({},{ sort: { _id: -1 } }, function(err, doc){
+        if(err){
+            res.send("Error is " + err);
+        }
+        else
+        {
+            if (doc) {
+                total_run = doc.total_run + total_run;
+                
+            }
+            else{
+                total_run = 0;
+            }
+            try{
+                data = { 
+                    team : "IND",
+                    newrun : score,
+                    total_run : total_run+score 
+                };
+                console.log(data);
+                 // insert the data into the database
+                 scoreCollection.insert(data, function(err, msg){
+                    if(err){
+                        res.send(err);
+                    }
+                    else{
+                        console.log('done');
+                    }
+                 })
+                }
+                catch(e){
+                    throw new Error('Some thing went wrong')
+                }
+            }
+        });
 
 })
 
 // over method
 router.post('/over_update', function(req, res){
-	var ball = req.body.ball;
-	res.send(ball);
+    var ball = Number(req.body.ball);
+    console.log(ball);
+
+    try{
+        switch(ball)
+        {
+            case 0: //correct ball
+            extra_run = 0;  
+            break;
+            case -1: //wide ball
+            extra_run = 1;   
+            break;
+            case -2:  //No ball
+            extra_run = 1;
+            default:
+            res.send("DATA is invalid");
+        }
+        scoreCollection.findOne({},{ sort: { _id: -1 } }, function(err, doc){
+            if(err){
+                res.send("Error is " + err);
+            }
+            else{
+                total_run = doc.total_run; //take the run add
+                data = {
+                    team : "IND",
+                    newrun : 0,
+                    total_run : total_run+extra_run,
+                    extra : ball
+                };
+                scoreCollection.insert(data, function(err, result){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        console.log(result);
+                    }
+                });
+            }
+        });
+    }
+    catch(e){
+        throw new Error('Some thing went wrong')
+    }
+    
 });
 module.exports = router;
