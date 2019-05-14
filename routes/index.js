35945be07b1f820fa5649c0eca9f26c2fcc2791c
airrakeshkumarsharma <airrakeshkumarsharma@gmail.com>
2019-05-14 14:17:08 +0530

    data received from admin side

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var monk = require('monk');  //get the monk javascript
 var moment = require('moment'); //get the moment data
 var dbs=monk('mongodb://admin:admin123@ds155916.mlab.com:55916/my_cricket_adda');
+var score 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -11,4 +12,15 @@ router.get('/admin', function(req, res, next) {
   res.render('admin');
 });
 
+//score post method
+router.post('/score_update', function(req, res){
+	var score = req.body.score;
+
+})
+
+// over method
+router.post('/over_update', function(req, res){
+	var ball = req.body.ball;
+	res.send(ball);
+});
 module.exports = router;
